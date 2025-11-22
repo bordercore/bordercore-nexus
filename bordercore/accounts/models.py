851a18ddd3d03f31e406da4e380efd0f653137a8
F. Jerrell Schivers <jerrell@bordercore.com>
2025-11-21 19:29:26 -0500

    Make NYTimes API key field optional in UserProfile model and form
    - Add blank=True to nytimes_api_key in UserProfile model
    - Remove required=False from form; rely on model field for optionality

--- bordercore/accounts/models.py
+++ bordercore/accounts/models.py
@@ -56,7 +56,7 @@ class UserProfile(models.Model):
     google_calendar = JSONField(blank=True, null=True)
     google_calendar_email = models.EmailField(blank=True, null=True)
     instagram_credentials = JSONField(blank=True, null=True)
-    nytimes_api_key = models.TextField(null=True)
+    nytimes_api_key = models.TextField(null=True, blank=True)
     homepage_default_collection = models.OneToOneField(Collection, related_name="default_collection", null=True, on_delete=models.PROTECT)
     homepage_image_collection = models.OneToOneField(Collection, related_name="image_collection", null=True, on_delete=models.PROTECT)
     sidebar_image = models.TextField(blank=True, null=True)
