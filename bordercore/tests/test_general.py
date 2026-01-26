59a8714c06634d3201a0da14bb0ea61639b2b67a
F. Jerrell Schivers <jerrell@bordercore.com>
2026-01-25 19:44:24 -0500

    Expand test_html to check React templates for inline styles and bad class names
    - Add support for scanning React (.jsx/.tsx) files for inline styles, className starting with '_', and _className attributes.
    - Include React template directory in test coverage.

--- bordercore/tests/test_general.py
+++ bordercore/tests/test_general.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import pytest
 from bs4 import BeautifulSoup
@@ -21,7 +22,8 @@ def test_html():
     template_list = []
     dir_list = [
         settings.TEMPLATES[0]["DIRS"],
-        [f"{os.environ['BORDERCORE_HOME']}/front-end/vue"]
+        [f"{os.environ['BORDERCORE_HOME']}/front-end/vue"],
+        [f"{os.environ['BORDERCORE_HOME']}/front-end/react"]
     ]
 
     for template_dir in ([item for sublist in dir_list for item in sublist]):
@@ -31,33 +33,56 @@ def test_html():
 
     for template in template_list:
 
-        with open(template, "r") as file:
-            page = file.read().replace("\n", "")
+        if template.endswith(".tsx") or template.endswith(".jsx"):
+            # React components
+            with open(template, "r") as file:
+                lines = file.readlines()
 
-        soup = BeautifulSoup(page, "lxml")
+            for i, line in enumerate(lines):
+                # Skip if current line or previous line has the exclusion comment
+                if "must remain inline" in line:
+                    continue
+                if i > 0 and "must remain inline" in lines[i-1]:
+                    continue
 
-        # Look for any inline CSS styles, ie a tag with a "style" attribute
-        count = soup.find_all(attrs={"style": True})
+                # Look for inline styles
+                assert not re.search(r"style=\{", line), f"Found inline CSS style in React component {template} at line {i+1}"
 
-        # If there is only one violation and it occurs in the "body"
-        #  tag, assume it's the inline background-image style,
-        #  which we're accepting for now for lack of a better place
-        #  to put it, given the STATIC_URL Django variable used.
-        if len(count) == 1 and "body" in [x.name for x in count]:
-            continue
+                # Look for className starting with underscore
+                assert not re.search(r"className=[\"'\{]\s*_", line), f"Found a class name starting with '_' in React component {template} at line {i+1}"
 
-        assert len(count) == 0, f"Found inline CSS style in template {template}"
+                # Look for _className attribute
+                assert not re.search(r"_className=", line), f"Found a tag with attribute name '_className' in React component {template} at line {i+1}"
 
-        # Look for any tag with a "_class" attribute
-        count = soup.find_all(attrs={"_class": True})
+        else:
+            # HTML templates (Django or Vue)
+            with open(template, "r") as file:
+                page = file.read().replace("\n", "")
 
-        assert len(count) == 0, f"Found a tag with attribute name '_class' in template {template}"
+            soup = BeautifulSoup(page, "lxml")
 
-        # Look for any class names that start with an underscore
-        divs = soup.select("div[class^='_']")
+            # Look for any inline CSS styles, ie a tag with a "style" attribute
+            count = soup.find_all(attrs={"style": True})
 
-        # This hack discards classes formed by Django conditional logic, which might contain
-        #  underscores that are perfectly fine.
-        count = [x for x in divs if "{%" not in x.attrs["class"]]
+            # If there is only one violation and it occurs in the "body"
+            #  tag, assume it's the inline background-image style,
+            #  which we're accepting for now for lack of a better place
+            #  to put it, given the STATIC_URL Django variable used.
+            if len(count) == 1 and "body" in [x.name for x in count]:
+                continue
 
-        assert len(count) == 0, f"Found a class name starting with '_' in template {template}"
+            assert len(count) == 0, f"Found inline CSS style in template {template}"
+
+            # Look for any tag with a "_class" attribute
+            count = soup.find_all(attrs={"_class": True})
+
+            assert len(count) == 0, f"Found a tag with attribute name '_class' in template {template}"
+
+            # Look for any class names that start with an underscore
+            divs = soup.select("div[class^='_']")
+
+            # This hack discards classes formed by Django conditional logic, which might contain
+            #  underscores that are perfectly fine.
+            count = [x for x in divs if "{%" not in x.attrs["class"]]
+
+            assert len(count) == 0, f"Found a class name starting with '_' in template {template}"
