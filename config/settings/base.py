e788669451bfa307f2a0e766d34e91f148d05151
F. Jerrell Schivers <jerrell@bordercore.com>
2026-03-01 12:39:20 -0500

    Increase API page size for bookmarks to 50

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated"
     ],
-    "PAGE_SIZE": 20
+    "PAGE_SIZE": 50
 }
 
 TEMPLATES = [
